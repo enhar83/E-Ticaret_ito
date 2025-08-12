0a572c3fd562cf85c5d56f0b1f3ec950d745cb39
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-08-12 16:14:38 +0300

    productdetails sayfasına similarproducts viewcomponenti eklendi

---
+++ Presentation/ViewComponents/SimilarProducts.cs
@@ -0,0 +1,24 @@
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.ViewComponents
+{
+    public class SimilarProducts:ViewComponent
+    {
+        private readonly IUnitOfWork _db;
+        public SimilarProducts(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke(Guid productId)
+        {
+            var product = _db.Products.GetById(productId);
+            
+            var similarProducts = _db.Products.GetAll(p => p.Category)
+                .Where(p => p.CategoryId == product.CategoryId && p.Id != product.Id)
+                .ToList();
+            return View(similarProducts);
+        }
+    }
+}
