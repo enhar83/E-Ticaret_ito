fe9e3696a9e59a81cb3e3d754f08422cff095405
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-08-18 13:10:22 +0300

    Identity yüklendi. AppRole ve AppUser sınıfları Entity katmanına eklendi.

---
+++ Entity/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Entity
+{
+    public class AppUser:IdentityUser<Guid>
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+}
---
+++ Presentation/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ViewComponents;
+
+namespace Presentation.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IUnitOfWork _db;
+
+        public AboutController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _db.Products.GetAll();
+            var categories = _db.Categories.GetAll();
+
+            ViewBag.ProductTotal = products.Count();
+            ViewBag.CategoryTotal = categories.Count();
+
+            return View();
+        }
+    }
+}
