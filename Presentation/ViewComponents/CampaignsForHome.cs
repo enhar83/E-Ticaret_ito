738d7cacf348e158aff23c4e76daa96abcb25e9c
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-09-05 11:29:06 +0300

    ana sayfa kampanya işlemleri yapıldı

---
+++ Presentation/ViewComponents/CampaignsForHome.cs
@@ -0,0 +1,21 @@
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.ViewComponents
+{
+    public class CampaignsForHome:ViewComponent
+    {
+        private readonly IUnitOfWork _db;
+
+        public CampaignsForHome(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var campaigns = _db.Campaigns.GetAll().Where(x=>x.IsActive==true).ToList();
+            return View(campaigns);
+        }
+    }
+}
